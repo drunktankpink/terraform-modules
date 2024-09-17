b625fcbd6961bacb3ace610201de999d012a9b5c
Declan Griffith-Barwell <declan.barwell@outlook.com>
2024-09-17 21:14:30 +0200

    enchancements to sg module

--- aws/security-group/main.tf
+++ aws/security-group/main.tf
@@ -5,19 +5,9 @@ locals {
 resource "aws_security_group" "this" {
   name        = local.security_group_name
   description = "${var.service_name} ${var.description} security group"
-  vpc_id      = var.vpc
+  vpc_id      = var.vpc_id
 
-  tags = {
+  tags = merge({
     Name = local.security_group_name
-  }
-}
-
-resource "aws_security_group_rule" "ingress" {
-  type              = "ingress"
-  from_port         = var.from_port
-  to_port           = var.to_port
-  protocol          = var.protocol
-  self              = local.cidr_blocks != true ? true : null
-  cidr_blocks       = local.cidr_blocks == true ? var.cidr_blocks : null
-  security_group_id = aws_security_group.this.id
+  }, var.tags)
 }
--- aws/security-group/variables.tf
+++ aws/security-group/variables.tf
@@ -8,27 +8,18 @@ variable description {
   type        = string
 }
 
-variable vpc {
+variable environment {
+  description = "Deployment environment (e.g., dev, prod)"
+  type        = string
+}
+
+variable vpc_id {
   description = "VPC ID for security group"
   type        = string
 }
 
-variable from_port {
-  description = "From port for security group rule"
-  type        = number
-}
-
-variable to_port {
-  description = "To port for security group rule"
-  type        = number
-}
-
-variable protocol {
-  description = "Protocol for security group rule"
-  type        = string
-}
-
-variable cidr_blocks {
-  description = "CIDR blocks for security group rules"
-  type        = list
+variable tags {
+  description = "Additional tags to apply to the security group"
+  type        = map(string)
+  default     = {}
 }
