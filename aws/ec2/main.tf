1001597bd965e3afee2dec7453147d31719ae38f
Declan Griffith-Barwell <declan.barwell@outlook.com>
2024-09-17 21:07:34 +0200

    enchancements to ec2 module

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,6 +1,36 @@
-data "aws_subnet" "this" {
+# Fetch latest Linux and Windows AMIs based on operating system
+data "aws_ami" "latest" {
+  most_recent = true
+  owners      = var.ami_is_map_owners[var.operating_system]
   filter {
-    name = "tag:SubnetTier"
-    values = var.pubpriv
+    name   = "name"
+    values = [var.ami_os_map_regex[var.operating_system]]
   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+# Provision EC2 instances based on the selected OS
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.latest.id
+  instance_type = var.instance_type
+  count         = var.instance_count
+  subnet_id     = data.aws_subnet.this.id
+  key_name      = var.key_pair
+
+  # Handle security groups dynamically based on OS
+  vpc_security_group_ids = var.operating_system == "windows" ? [aws_security_group.rdp.id] : [aws_security_group.ssh.id]
+
+  # Dynamic user data script for bootstrapping instances
+  user_data = var.operating_system == "windows" ? file("scripts/windows_bootstrap.ps1") : file("scripts/linux_bootstrap.sh")
+
+  tags = merge({
+    Name = "${var.service_name}-${var.environment}-${var.operating_system}"
+  }, var.tags)
 }
--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -8,6 +8,11 @@ variable description {
   type        = string
 }
 
+variable environment {
+  description = "Deployment environment (e.g., dev, prod)"
+  type        = string
+}
+
 variable instance_type {
   description = "The instance type"
   type = string
@@ -26,6 +31,11 @@ variable root_volume_size {
   default = 8
 }
 
+variable key_pair {
+  description = "Name of the key pair to use for SSH or RDP"
+  type        = string
+}
+
 variable backup_enabled {
   description = "AWS Backup enabled or disabled"
   type = bool
@@ -34,20 +44,21 @@ variable backup_enabled {
 
 # AMI search
 variable operating_system {
-  description = "The operating system to use"
-  type = string
-  default = "amazon"
+  description = "The operating system to use (windows, amazon-linux, ubuntu, etc.)"
+  type        = string
+  default     = "amazon-linux"
 }
 
 variable ami_os_map_regex {
   description = "Map of regex to search amis"
-  type = map(string)
+  type        = map(string)
   default = {
-    amazon  = value
-    kali    = value
-    rhel    = value
-    suse    = value
-    windows = value
+    amazon-linux = "amzn2-ami-hvm-*-x86_64-gp2",
+    ubuntu       = "ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*",
+    windows      = "Windows_Server-2019-English-Full-Base-*"
+    kali         = "value",
+    rhel         = "value",
+    suse         = "value"
   }
 }
 
@@ -55,10 +66,17 @@ variable ami_is_map_owners {
   description = "Map of ami owners to filter only official amis"
   type = map(list)
   default = {
-    amazon  = value
-    kali    = value
-    rhel    = value
-    suse    = value
-    windows = value
+    amazon-linux = ["137112412989"],
+    ubuntu       = ["099720109477"],
+    windows      = ["801119661308"],
+    kali         = ["000000000000"],
+    rhel         = ["000000000000"],
+    suse         = ["000000000000"]
   }
+}
+
+variable tags {
+  description = "Additional tags to apply to resources"
+  type        = map(string)
+  default     = {}
 }
