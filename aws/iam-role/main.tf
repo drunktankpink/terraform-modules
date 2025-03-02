58a8676f0d7dc1b5cd08539ab10592f47e1a0533
Declan Griffith-Barwell <declan.barwell@outlook.com>
2025-03-02 17:32:16 +1100

    added iam-role module

--- aws/iam-role/main.tf
+++ aws/iam-role/main.tf
@@ -1,18 +1,20 @@
-data "aws_caller_identity" "current" {}
-
 locals {
-  iam_policy_name = "${var.service_name}-${var.description}-iam-policy"
   iam_role_name = "${var.service_name}-${var.description}-iam-role"
 }
 
-resource "aws_iam_policy" "this" {
-  name = local.iam_policy_name
-  path = var.policy_path
-  policy = var.iam_policy
+resource "aws_iam_role" "this" {
+  name = local.iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 
-  tags = {
-    Name = local.iam_policy_name
-  }
+  tags = merge(
+    {
+      Name        = local.iam_role_name
+      Environment = var.environment
+      Project     = var.project_name
+      Owner       = var.owner
+    },
+    var.additional_tags
+  )
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -21,21 +23,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
     principals {
       type = "Service"
-      identifiers = ["${var.aws_service}.amazonaws.com"]
+      identifiers = var.assume_role_principals
     }
   }
-}
-
-resource "aws_iam_role" "this" {
-  name = local.iam_role_name
-  force_detatch_policy = data.aws_iam_policy_document.assume_role_policy.json
-
-  tags = {
-    Name = local.iam_role_name
-  }
-}
-
-resource "aws_iam_policy_attachment" "this" {
-  role = aws_iam_role.this.id
-  policy_arn = aws_iam_policy.this.arn
 }
--- aws/iam-role/variables.tf
+++ aws/iam-role/variables.tf
@@ -8,18 +8,28 @@ variable description {
   type        = string
 }
 
-variable iam_policy {
-  description = "IAM Policy"
-  type = string
+variable "environment" {
+  description = "Environment (e.g. dev, test, prod)"
+  type        = string
 }
 
-variable policy_path {
-  description = "Path for policy. Defaults to /"
-  type = string
-  default = "/"
+variable "project_name" {
+  description = "The project this IAM role is associated with"
+  type        = string
 }
 
-variable aws_service {
-  description = "AWS service name"
-  type = string
+variable "assume_role_principals" {
+  description = "List of services allowed to assume this role (e.g., 'ec2.amazonaws.com')"
+  type        = list(string)
+}
+
+variable "owner" {
+  description = "Owner of the IAM role"
+  type        = string
+}
+
+variable "additional_tags" {
+  description = "Additional tags to apply to the IAM role"
+  type        = map(string)
+  default     = {}
 }
