471abef549b315b2f63fade34059d098e9038acf
Declan Griffith-Barwell <declan.barwell@outlook.com>
2025-03-02 20:39:23 +1100

    Security Group: Updated module

--- aws/security-group/main.tf
+++ aws/security-group/main.tf
@@ -1,13 +1,20 @@
 locals {
-  resource_name = "${var.service_name}-${var.description}-sg"
+  security_group_name        = "${var.service_name}-${var.description}-sg"
+  security_group_description = "${var.service_name} ${var.description} security group"
 }
 
 resource "aws_security_group" "this" {
-  name        = local.resource_name
-  description = "${var.service_name} ${var.description} security group"
+  name        = local.security_group_name
+  description = local.security_group_description
   vpc_id      = var.vpc_id
 
-  tags = merge({
-    Name = local.resource_name
-  }, var.tags)
+  tags = merge(
+    {
+      Name        = local.security_group_name
+      Environment = var.environment
+      Project     = var.project_name
+      Owner       = var.owner
+    },
+    var.additional_tags
+  )
 }
