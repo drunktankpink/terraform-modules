44183fdc347e8cf3eab8675e94efc1023e10237d
Declan Griffith-Barwell <declan.barwell@outlook.com>
2025-03-02 17:33:45 +1100

    iam-role: remove redundant entries in output.tf

--- aws/iam-role/outputs.tf
+++ aws/iam-role/outputs.tf
@@ -1,18 +1,3 @@
-output policy_id {
-  description = "The policy's ID"
-  value       = aws_iam_policy.this.id
-}
-
-output policy_arn {
-  description = "The policy's ARN"
-  value       = aws_iam_policy.this.arn
-}
-
-output policy_name {
-  description = "The policy's name"
-  value       = aws_iam_policy.this.name
-}
-
 output role_arn {
   description = "The IAM role ARN"
   value       = aws_iam_role.this.arn
