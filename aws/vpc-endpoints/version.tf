087e02519b7cf324af6eccbb2935cbbff74d4c23
Declan Griffith-Barwell <declan.barwell@outlook.com>
2025-03-02 20:40:27 +1100

    VPC-Endpoints: Added versions.tf

--- aws/vpc-endpoints/version.tf
+++ aws/vpc-endpoints/version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= v0.15.3"
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">= 3.47.0"
+    required_version = ">= v0.15.3"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 4.20.0"
+        }
     }
-  }
 }
