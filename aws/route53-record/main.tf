8c2c854a54361556110874d3bd5bb553d6e04d21
Declan Griffith-Barwell <declan.barwell@outlook.com>
2025-03-03 15:44:50 +1100

    added r53 record module

---
+++ aws/route53-record/main.tf
@@ -0,0 +1,16 @@
+
+resource "aws_route53_record" "this" {
+  for_each = var.records
+
+  zone_id = var.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = each.value.ttl
+  records = each.value.records
+
+  alias {
+    name                   = each.value.alias_name
+    zone_id                = each.value.alias_zone_id
+    evaluate_target_health = each.value.evaluate_target_health
+  }
+}
