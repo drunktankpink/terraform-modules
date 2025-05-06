afb192d3c2e98f768bcab772be4868002c590841
Declan Griffith-Barwell <37308847+drunktankpink@users.noreply.github.com>
2025-05-06 12:40:59 +1000

    aws/rds - updated depreciated name attribute to db_name

--- aws/rds/main.tf
+++ aws/rds/main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "this" {
   storage_type         = var.storage_type
   engine               = var.engine
   instance_class       = var.instance_class
-  name                 = var.db_name
+  db_name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = var.parameter_group_name
